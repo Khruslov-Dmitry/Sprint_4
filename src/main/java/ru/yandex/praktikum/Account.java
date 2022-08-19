50a8323a0176f5cc187513cbba2af9a09e367ae6
Dmitry Khruslov <dem6n@yandex.ru>
2022-08-19 09:10:36 +0300

    Account&Praktikum classes done

--- src/main/java/ru/yandex/praktikum/Account.java
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -1,5 +1,7 @@
 package ru.yandex.praktikum;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Account {
 
     private final String name;
@@ -13,6 +15,12 @@ public class Account {
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
+        if (name.startsWith(" ") || name.endsWith(" ")) {
+            return false;
+        }
+        else if (name.length() < 3 || name.length() > 19) {
+            return false;
+        }
+        else return StringUtils.countMatches(name, " ") == 1;
     }
-
 }
